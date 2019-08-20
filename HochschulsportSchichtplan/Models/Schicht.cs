71fba12ed6fd724e7af8f627d76004e9f1a5ac26
Lennart Brückner <lennart.brueckner@googlemail.com>
2019-08-20 14:48:04 +0200

    test

---
+++ HochschulsportSchichtplan/Models/Schicht.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HochschulsportSchichtplan.Models
+{
+    public class Schicht
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Tag { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Start{ get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Ende { get; set; }
+
+        public String Inhaber{ get; set; }
+        public Double Stunden { get; set; }
+    }
+}
+
