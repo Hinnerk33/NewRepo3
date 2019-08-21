384faad902c5031376b05b64b55fafc38a09a187
Lennart Brückner <lbrueckner15@outlook.de>
2019-08-21 17:00:16 +0200

    schichtplan hinzugefügt

---
+++ HochschulsportSchichtplan/Controllers/SchichtplanController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HochschulsportSchichtplan.Models;
+using Microsoft.IdentityModel;
+using HochschulsportSchichtplan.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HochschulsportSchichtplan.Controllers
+{
+    public class SchichtplanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- HochschulsportSchichtplan/Models/Schicht.cs
+++ HochschulsportSchichtplan/Models/Schicht.cs
@@ -12,6 +12,7 @@ namespace HochschulsportSchichtplan.Models
         public int Id { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Tag { get; set; }
 
         [DataType(DataType.Time)]
