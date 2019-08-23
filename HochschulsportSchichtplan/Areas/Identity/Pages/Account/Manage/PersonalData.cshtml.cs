7bbbd52a5fb74397a3fe26cc46aa6f64a0fc06f8
Lennart Brückner <lennart.brueckner@googlemail.com>
2019-08-23 22:39:57 +0200

    versuch Schichtplan

--- HochschulsportSchichtplan/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ HochschulsportSchichtplan/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -4,16 +4,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using HochschulsportSchichtplan.Models;
 
 namespace HochschulsportSchichtplan.Areas.Identity.Pages.Account.Manage
 {
     public class PersonalDataModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<PersonalDataModel> _logger;
 
         public PersonalDataModel(
-            UserManager<IdentityUser> userManager,
+            UserManager<ApplicationUser> userManager,
             ILogger<PersonalDataModel> logger)
         {
             _userManager = userManager;
